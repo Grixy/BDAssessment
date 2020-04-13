94d63c39e9ffae9eb4b2edc47e8fe73044632bb7
Grixy <ruangates@gmail.com>
2020-04-13 14:07:00 +0200

    Initial Commit

---
+++ BD_Assessment_WebAPI_Ruan_Gates/BD_Assessment_WebAPI_Ruan_Gates/Models/Batch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BD_Assessment_WebAPI_Ruan_Gates.Models
+{
+	public class Batch
+	{
+		[Key]
+		public int BatchId { get; set; }
+
+		[Required]
+		[Column(TypeName = "varchar(16)")]
+		public string CardNumber { get; set; }
+	}
+
+
+}
---
+++ BD_Assessment_WebAPI_Ruan_Gates/BD_Assessment_WebAPI_Ruan_Gates/Models/BatchContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BD_Assessment_WebAPI_Ruan_Gates.Models
+{
+	public class BatchContext
+	{
+	}
+}
